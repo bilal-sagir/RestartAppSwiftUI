49f2dc2b00c8adccee4ef1dd27c819f02b62a8c3
bilal-sagir <sagir.bilal@icloud.com>
2024-02-20 12:31:01 +0300

    Initial Commit

---
+++ RestartAppSwiftUI/RestartAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppSwiftUIApp.swift
+//  RestartAppSwiftUI
+//
+//  Created by Bilal on 20.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
