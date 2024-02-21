0a3719c5234b53041b81d694bfb276f7c9e169e8
bilal-sagir <sagir.bilal@icloud.com>
2024-02-21 19:15:57 +0300

    added home screen

---
+++ RestartAppSwiftUI/Views/CircleGroupView.swift
@@ -0,0 +1,35 @@
+//
+//  CircleGroupView.swift
+//  RestartAppSwiftUI
+//
+//  Created by Bilal on 21.02.2024.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    
+    // MARK: - PROPERTIES
+    @State var shapeColor: Color
+    @State var shapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(shapeColor.opacity(shapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(shapeColor.opacity(shapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color("ColorBlue")
+            .ignoresSafeArea()
+        CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+    }
+    
+}
