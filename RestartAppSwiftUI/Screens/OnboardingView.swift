cb618b5d9e18bd4aa729d699a7590ef41594ca24
bilal-sagir <sagir.bilal@icloud.com>
2024-02-24 17:35:55 +0300

    added bunch of animations to onboarding screen

--- RestartAppSwiftUI/Screens/OnboardingView.swift
+++ RestartAppSwiftUI/Screens/OnboardingView.swift
@@ -14,6 +14,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share."
     
     var body: some View {
         ZStack {
@@ -25,10 +28,12 @@ struct OnboardingView: View {
                 //MARK: - HEADER
                 Spacer()
                 VStack(spacing: 0) {
-                    Text("Share.")
+                    Text(textTitle)
                         .fontWeight(.heavy)
                         .font(.system(size: 60))
                         .foregroundStyle(.white)
+                        .transition(.opacity)
+                        .id(textTitle) // added to avoid the wierd text shaking bug after transition
                     
                     Text("""
 It's not how much we give but
@@ -49,13 +54,51 @@ how much love we put into giving.
                 
                     CircleGroupView(shapeColor: .white,
                                     shapeOpacity: 0.2)
+                    .offset(x: imageOffset.width * -1)
+                    .blur(radius: abs(imageOffset.width / 5))
+                    .animation(.easeOut(duration: 1), value: imageOffset)
+                    
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.75), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                    }
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 0
+                                        textTitle = "Give."
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    imageOffset = .zero
+                                    
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Share."
+                                    }
+                                })
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                 } //: CENTER
+                
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    , alignment: .bottom
+                )
                 Spacer()
                 
                 // MARK: - FOOTER
